9c76ab1ad9efd93a72138d67a4daa2c8693a624f
Tauan Lima <contato@tauanlima.com.br>
2024-09-02 17:33:30 -0300

    Create GLPI-Install

---
+++ DB-Percona/Volumas.sh
@@ -0,0 +1,3 @@
+mkdir -p /data/glpi-10/percona/lib
+mkdir -p /data/glpi-10/percona/log
+chown 1001:0 -R /data/glpi-10/percona
---
+++ GLPI/Volumes.sh
@@ -0,0 +1,9 @@
+mkdir -p /data/glpi-10/glpi/documents
+mkdir -p /data/glpi-10/glpi/marketplace
+mkdir -p /data/glpi-10/glpi/plugins
+mkdir -p /data/glpi-10/glpi/files/_pictures
+mkdir -p /data/glpi-10/glpi/files/_plugins
+mkdir -p /data/glpi-10/glpi/etc
+mkdir -p /data/glpi-10/glpi/imagens-custom
+mkdir -p /data/glpi-10/backup
+chown 70:70 -R /data/glpi-10/glpi
